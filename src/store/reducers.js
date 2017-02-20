2cd3f28224f33780636b392e527f9ca5da4e8d87
Kushan Joshi <0o3ko0@gmail.com>
2017-02-20 13:21:48 +0530

    template for  filter actions

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,6 +3,18 @@ import {combineReducers} from 'redux';
 
 function stats(state = {}, action) {
     switch (action.type) {
+    case 'SET_USER_FILTER': {
+        return filterUsers(state, action.filter);
+    }
+    case 'SET_TAG_FILTER': {
+        return filterTags(state, action.filter);
+    }
+    case 'SET_TIME_FILTER': {
+        return filterTime(state, action.filter);
+    }
+    case 'SET_BBOX_FILTER': {
+        return filterBbox(state, action.filter);
+    }
     case 'RECEIVE_STATS': {
         return {
             ...state,
@@ -17,6 +29,32 @@ function stats(state = {}, action) {
             rawData: []
         };
     }
+
 }
 
+function filterUsers(state, filter) {
+
+    return {
+        ...state
+    }
+}
+
+function filterTags(state, filter) {
+    return {
+        ...state
+    }
+}
+
+
+function filterTime(state, filter) {
+    return {
+        ...state
+    }
+}
+
+function filterBbox(state, filter) {
+    return {
+        ...state
+    }
+}
 export default combineReducers({stats});
