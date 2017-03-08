dce7db620c8c2e0b7978aecfdcbd0292b3439e86
Kushan Joshi <0o3ko0@gmail.com>
2017-03-08 15:25:41 +0530

    Updates

--- src/components/PillButton.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export function PillButton ({active, onClick, classes, children}) {
-    return (
-        <button onClick={onClick} className={`${active ? 'is-active' : '' } btn btn--pill btn--s color-gray btn--gray-faint ${classes}`}>{children}</button>
-    );
-}
